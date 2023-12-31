342059d6f9b8bc0a02fd6ef86aac1a6d0a6d9813
Osman Emre Gundogdu <emreguendogdu@yandex.com>
2023-12-31 14:14:59 +0300

    footer rain anim fail

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,18 +1,53 @@
 import "./Footer.css"
 import GitHubIcon from "../../assets/githubicon.svg"
 import LinkedInIcon from "../../assets/linkedinicon.svg"
+import { useEffect } from "react"
+
 export default function Footer() {
+  useEffect(() => {
+    const briefcases = document.querySelectorAll(".js-briefcase")
+    briefcases.forEach((briefcase) => {
+      const randomNumber = Math.floor(Math.random() * (100 - 50) + 50)
+      const randomNegativeNumber = Math.floor(Math.random() * (75 - 125) - 125)
+
+      briefcase.style = `--rain-anim1: ${randomNegativeNumber}; --rain-anim2: ${randomNumber};`
+    })
+  })
+
+  const rainAnimation = () => {
+    const briefcases = document.querySelectorAll(".js-briefcase")
+    briefcases.forEach((briefcase) => {
+      briefcase.style.opacity = 1
+      briefcase.classList.add("rain")
+    })
+  }
   return (
     <footer>
-      <a target="_blank" rel="noreferrer" href="https://github.com/osmangund">
+      <a
+        id="github-icon"
+        target="_blank"
+        rel="noreferrer"
+        href="https://github.com/osmangund"
+      >
         <img src={GitHubIcon} alt="GitHub" className="icon" />
       </a>
       <a
         target="_blank"
         rel="noreferrer"
         href="https://linkedin.com/in/osmangund"
+        onMouseOver={() => rainAnimation()}
       >
         <img src={LinkedInIcon} alt="LinkedIn" className="icon" />
+        <span className="js-briefcase">💼</span>
+        <span className="js-briefcase">💼</span>
+        <span className="js-briefcase">💼</span>
+        <span className="js-briefcase">💼</span>
+        <span className="js-briefcase">💼</span>
+        <span className="js-briefcase">💼</span>
+        <span className="js-briefcase">💼</span>
+        <span className="js-briefcase">💼</span>
+        <span className="js-briefcase">💼</span>
+        <span className="js-briefcase">💼</span>
       </a>
     </footer>
   )
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,34 +3,42 @@ export default function Home() {
   return (
     <>
       <header id="home-header">
-        <h5>osmangund®</h5>
+        <a
+          id="title"
+          href="
+        https://www.linkedin.com/in/osmangund/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          osmangund®
+        </a>
       </header>
       <main id="home">
-        <article>
+        <article style={{ "--order": 1 }}>
           <a href="/#/barchart">
             <h3 className="title">Bar Chart</h3>
             <p className="description">US gross domestic product graph</p>
           </a>
         </article>
-        <article>
+        <article style={{ "--order": 2 }}>
           <a href="/#/choropleth">
             <h3 className="title">Choropleth</h3>
             <p className="description">US educational attainment graph</p>
           </a>
         </article>
-        <article>
+        <article style={{ "--order": 3 }}>
           <a href="/#/heatmap">
             <h3 className="title">Heat Map</h3>
             <p className="description">Global land surface temperature graph</p>
           </a>
         </article>
-        <article>
+        <article style={{ "--order": 4 }}>
           <a href="/#/treemap">
             <h3 className="title">Tree Map</h3>
             <p className="description">Three graphs in distinct categories</p>
           </a>
         </article>
-        <article>
+        <article style={{ "--order": 5 }}>
           <a href="/#/scatterplot">
             <h3 className="title">Scatter Plot</h3>
             <p className="description">Doping in cycling scatter graph</p>
