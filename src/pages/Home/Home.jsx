972a3593773ae647f177cc3cc01e45d75804f2a6
Osman Emre Gundogdu <emreguendogdu@yandex.com>
2024-01-02 02:43:44 +0300

    Update & fix links

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import "./Home.css"
 export default function Home() {
+
   return (
     <>
       <header id="home-header">
@@ -15,31 +16,31 @@ export default function Home() {
       </header>
       <main id="home">
         <article style={{ "--order": 1 }}>
-          <a href="/barchart">
+          <a href="/data-visualization-projects/#/barchart">
             <h3 className="title">Bar Chart</h3>
             <p className="description">US gross domestic product graph</p>
           </a>
         </article>
         <article style={{ "--order": 2 }}>
-          <a href="/choropleth">
+          <a href="/data-visualization-projects/#/choropleth">
             <h3 className="title">Choropleth</h3>
             <p className="description">US educational attainment graph</p>
           </a>
         </article>
         <article style={{ "--order": 3 }}>
-          <a href="/heatmap">
+          <a href="/data-visualization-projects/#/heatmap">
             <h3 className="title">Heat Map</h3>
             <p className="description">Global land surface temperature graph</p>
           </a>
         </article>
         <article style={{ "--order": 4 }}>
-          <a href="/treemap">
+          <a href="/data-visualization-projects/#/treemap">
             <h3 className="title">Tree Map</h3>
             <p className="description">Three graphs in distinct categories</p>
           </a>
         </article>
         <article style={{ "--order": 5 }}>
-          <a href="/scatterplot">
+          <a href="/data-visualization-projects/#/#/scatterplot">
             <h3 className="title">Scatter Plot</h3>
             <p className="description">Doping in cycling scatter graph</p>
           </a>
